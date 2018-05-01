5799e2a1485da56b9d17e9e891663e27b308710a
Yukari Nakanishi <y.nakanishi@opt.ne.jp>
2018-05-01 10:26:25 +0900

    ボタン一旦削除。コンピューターの結果を出す場所を定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import './App.css';
 
-class App extends React.Component {
+interface CompState {
+    value: string;
+}
+
+class App extends React.Component<{comp: string}, CompState> {
   render() {
     return (
       <div className="App">
@@ -9,10 +13,11 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <ul>
-          <li><button type=button onclick="alert('グー')">グー</button></li>
-          <li><button type=button onclick="alert('チョキ')">チョキ</button></li>
-          <li><button type=button onclick="alert('パー')">パー</button></li>
+          <li>グー</li>
+          <li>チョキ</li>
+          <li>パー</li>
         </ul>
+        <div>{this.props.comp}</div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
